2be3cfc0c8064508f5df92fbccbc1c297c59f7c1
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-14 15:57:44 -0500

    Iniciando microservicio item

---
+++ msvc-item/src/main/java/org/personales/msvcitem/MsvcItemApplication.java
@@ -0,0 +1,13 @@
+package org.personales.msvcitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcItemApplication.class, args);
+	}
+
+}
---
+++ msvc-item/src/test/java/org/personales/msvcitem/MsvcItemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.msvcitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
