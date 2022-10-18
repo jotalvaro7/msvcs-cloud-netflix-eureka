a3d3685a4e31037e13f54acdbc3303406561739f
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-18 14:52:51 -0500

    Filtros Gateway Factory que vienen de fabrica

--- msvc-item/src/main/java/org/personales/msvcitem/web/controller/ItemController.java
+++ msvc-item/src/main/java/org/personales/msvcitem/web/controller/ItemController.java
@@ -5,9 +5,7 @@ import org.personales.msvcitem.domain.dto.ItemDTO;
 import org.personales.msvcitem.domain.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +17,11 @@ public class ItemController {
     private ItemService itemService;
 
     @GetMapping("/listar")
-    public List<ItemDTO> getAllItems(){
+    public List<ItemDTO> getAllItems(
+            @RequestParam(name = "nombre", required = false) String nombre,
+            @RequestHeader(name = "token-request", required = false) String token){
+        System.out.println(nombre);
+        System.out.println(token);
         return itemService.getAll();
     }
 
