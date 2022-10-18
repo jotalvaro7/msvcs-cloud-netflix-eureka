e496be59bbf83a93a6022a4448e59002df1e032d
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-18 14:08:47 -0500

    Implementando Gateway filter factory personalizado

--- gateway-server/src/main/java/org/personales/gatewayserver/filters/factory/EjemploGatewayFilterFactory.java
+++ gateway-server/src/main/java/org/personales/gatewayserver/filters/factory/EjemploGatewayFilterFactory.java
@@ -1,2 +1,72 @@
-package org.personales.gatewayserver.filters.factory;public class EjemploGatewayFilterFactory {
+package org.personales.gatewayserver.filters.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@Component
+public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<EjemploGatewayFilterFactory.Configuracion> {
+
+    private final Logger log = LoggerFactory.getLogger(EjemploGatewayFilterFactory.class);
+
+    public EjemploGatewayFilterFactory() {
+        super(Configuracion.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Configuracion config) {
+        return (exchange, chain) -> {
+            log.info("ejecutando pre-filtro factory: " + config.mensaje);
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+                Optional.ofNullable(config.cookieValor).ifPresent(cookie -> {
+                    exchange.getResponse().addCookie(ResponseCookie.from(config.cookieNombre, cookie).build());
+                });
+
+                log.info("ejecutando post-filtro factory: " + config.mensaje);
+            }));
+        };
+    }
+
+    @Override
+    public String name() {
+        return "EjemploCookie";
+    }
+
+    public static class Configuracion {
+        private String mensaje;
+        private String cookieValor;
+        private String cookieNombre;
+
+        public String getMensaje() {
+            return mensaje;
+        }
+
+        public void setMensaje(String mensaje) {
+            this.mensaje = mensaje;
+        }
+
+        public String getCookieValor() {
+            return cookieValor;
+        }
+
+        public void setCookieValor(String cookieValor) {
+            this.cookieValor = cookieValor;
+        }
+
+        public String getCookieNombre() {
+            return cookieNombre;
+        }
+
+        public void setCookieNombre(String cookieNombre) {
+            this.cookieNombre = cookieNombre;
+        }
+    }
+
 }
