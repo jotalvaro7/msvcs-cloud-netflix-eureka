ce73cbcd2825aba7c7a88380356699237051f60a
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-18 21:04:46 -0500

    Configurando microservicio items con Resilience4J

--- msvc-item/src/main/java/org/personales/msvcitem/web/controller/ItemController.java
+++ msvc-item/src/main/java/org/personales/msvcitem/web/controller/ItemController.java
@@ -2,15 +2,20 @@ package org.personales.msvcitem.web.controller;
 
 
 import org.personales.msvcitem.domain.dto.ItemDTO;
+import org.personales.msvcitem.domain.dto.ProductoDTO;
 import org.personales.msvcitem.domain.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ItemController {
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier("serviceFeign")
@@ -27,7 +32,19 @@ public class ItemController {
 
     @GetMapping("/listar/{id}/cantidad/{cantidad}")
     public ItemDTO getItemById(@PathVariable Long id, @PathVariable Integer cantidad){
-        return itemService.getItemById(id, cantidad);
+        return cbFactory.create("items")
+                .run(() -> itemService.getItemById(id, cantidad), throwable -> metodoAlternativo(id, cantidad));
+    }
+
+    public ItemDTO metodoAlternativo(Long id, Integer cantidad){
+        ItemDTO itemDTO = new ItemDTO();
+        ProductoDTO producto = new ProductoDTO();
+        itemDTO.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        itemDTO.setProductoDTO(producto);
+        return itemDTO;
     }
 
 }
