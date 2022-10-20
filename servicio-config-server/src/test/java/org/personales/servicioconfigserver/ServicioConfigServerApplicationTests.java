78c24493f0e502cedb372e26d26699b5b37bcf4f
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-10-19 21:50:39 -0500

    Creando servidor de configuracion

---
+++ servicio-config-server/src/test/java/org/personales/servicioconfigserver/ServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.servicioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
