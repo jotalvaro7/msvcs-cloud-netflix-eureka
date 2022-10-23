cff3c7966ea5e9e1b483ff2c0854691fb927d5d8
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-10-23 14:50:22 -0500

    creando microservicio usuarios

---
+++ servicio-usuarios/src/test/java/org/personales/serviciousuarios/ServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.serviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
