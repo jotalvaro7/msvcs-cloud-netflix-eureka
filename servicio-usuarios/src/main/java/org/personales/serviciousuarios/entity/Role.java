1b4c1ff486dfefe722ebd51197ad8ea0d7f5eea4
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-10-24 12:02:09 -0500

    Creando las entidades necesarias Usuario y Role, la relacion de muchos a muchos entre Usuario y Role

---
+++ servicio-usuarios/src/main/java/org/personales/serviciousuarios/entity/Role.java
@@ -0,0 +1,25 @@
+package org.personales.serviciousuarios.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    //generated serial version UID
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(unique = true, length = 30)
+    private String nombre;
+
+
+}
