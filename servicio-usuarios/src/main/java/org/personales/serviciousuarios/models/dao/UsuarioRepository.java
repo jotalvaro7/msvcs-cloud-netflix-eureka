2647db66b36a8d0959e559f2a785b65288a7d87f
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-10-24 15:45:37 -0500

    Exportar metodos repositorio rest con endpoint search

--- servicio-usuarios/src/main/java/org/personales/serviciousuarios/models/dao/UsuarioRepository.java
+++ servicio-usuarios/src/main/java/org/personales/serviciousuarios/models/dao/UsuarioRepository.java
@@ -3,12 +3,15 @@ package org.personales.serviciousuarios.models.dao;
 import org.personales.serviciousuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
 
-    Usuario findByUsername(String username);
+    @RestResource(path = "buscar-username")
+    Usuario findByUsername(@Param("username") String username);
 
     //Consulta con Query nativa de SQL para buscar por username igual que el metodo anterior pero con Query nativa de SQL
     /*
